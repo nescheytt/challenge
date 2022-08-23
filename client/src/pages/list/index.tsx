1e9879bb114696ade4bd7d2b02993439b1539d26
nescheytt <nahuel.scheytt@gmail.com>
2022-08-23 00:18:58 -0300

    Page List - Add component breadcrumb into list

--- client/src/pages/list/index.tsx
+++ client/src/pages/list/index.tsx
@@ -1,48 +1,22 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import axios from 'axios'
-import logo from '../../assets/images/logo.svg'
+import React from 'react'
+
+// components
+import Layout from '../../components/layouts'
+import Breadcrumb from '../../components/breadcrumb'
+
+// styles
 import './styles.scss'
 import '../../assets/scss/grid/container.scss'
 import '../../assets/scss/grid/row.scss'
 import '../../assets/scss/grid/col.scss'
-import Layout from '../../components/layouts'
-
-interface DataProps {
-  exampleMessage: string
-}
 
 const List = () => {
-  const [data, setData] = useState<DataProps>()
-
-  const fetchData = useCallback(async () => {
-    axios
-      .get("http://localhost:4000/")
-      .then((response) => setData(response.data))
-  }, [])
-
-  useEffect(() => {
-    fetchData()
-  }, [fetchData])
-
   return (
     <Layout>
-      <div className="App container fluid">
+      <div className="container fluid">
         <div className="row">
           <div className="col-12">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {data?.exampleMessage}
-              </a>
-            </header>
+            <Breadcrumb />
           </div>
         </div>
       </div>
