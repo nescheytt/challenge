fb1b69f9132ee12d55e50942260ea7e11fb491b3
nescheytt <nahuel.scheytt@gmail.com>
2022-08-22 21:49:39 -0300

    Add search bar component inside header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -5,16 +5,19 @@ import './styles.scss'
 
 // images
 import LogoMeli from '../../assets/images/logo_meli.png'
+import SearchBar from '../search-bar'
 
 const Header = () => {
   return (
     <div className="header">
       <div className="container fluid">
         <div className="row">
-          <div className="col-12">
-            <div className="flex">
-              <img src={LogoMeli} alt="logo-meli" />
-            </div>
+          <div className="col-1">
+            <img src={LogoMeli} alt="logo-meli" width="auto" height="auto" />
+          </div>
+
+          <div className="col-11">
+            <SearchBar />
           </div>
         </div>
       </div>
